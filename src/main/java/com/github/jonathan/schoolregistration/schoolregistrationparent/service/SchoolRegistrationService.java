92510ee29ea755ab4819492a4ef6f49ddfe78311
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2022-02-15 17:09:54 -0500

    added business logic for registration

--- src/main/java/com/github/jonathan/schoolregistration/schoolregistrationparent/service/SchoolRegistrationService.java
+++ src/main/java/com/github/jonathan/schoolregistration/schoolregistrationparent/service/SchoolRegistrationService.java
@@ -1,25 +1,74 @@
 package com.github.jonathan.schoolregistration.schoolregistrationparent.service;
 
 import com.github.jonathan.schoolregistration.schoolregistrationparent.dao.CourseRepository;
+import com.github.jonathan.schoolregistration.schoolregistrationparent.dao.RegistrationRepository;
 import com.github.jonathan.schoolregistration.schoolregistrationparent.dao.StudentRepository;
 import com.github.jonathan.schoolregistration.schoolregistrationparent.domain.Course;
+import com.github.jonathan.schoolregistration.schoolregistrationparent.domain.Registration;
 import com.github.jonathan.schoolregistration.schoolregistrationparent.domain.Student;
+import com.github.jonathan.schoolregistration.schoolregistrationparent.exception.CannotCreateRegistrationException;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Getter
 @Setter
+@Slf4j
 public class SchoolRegistrationService {
+
+    @Value("${schoolRegistrationService.maximumNumberOfStudentsPerCourse:50}")
+    private int maximumNumberOfStudentsPerCourse;
+    @Value("${schoolRegistrationService.maximumNumberOfCoursesPerStudent:5}")
+    private int maximumNumberOfCoursesPerStudent;
+
     @Autowired
     private CourseRepository courseRepository;
     @Autowired
     private StudentRepository studentRepository;
+    @Autowired
+    private RegistrationRepository registrationRepository;
+
+    public Registration createRegistration(Long studentId, Long courseId) throws CannotCreateRegistrationException {
+        final Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        final Optional<Course> optionalCourse = courseRepository.findById(courseId);
+        if (optionalStudent.isPresent() && optionalCourse.isPresent()) {
+            return createRegistration(optionalStudent.get(), optionalCourse.get());
+        }
+        throw new CannotCreateRegistrationException(studentId, courseId);
+    }
+
+    public Registration createRegistration(Student student, Course course) throws CannotCreateRegistrationException {
+        if (studentCanRegisterToCourse(student, course)) {
+            return registrationRepository.save(
+                    Registration.builder().student(student).course(course).build()
+            );
+        }
+        throw new CannotCreateRegistrationException(student, course);
+    }
+
+    protected boolean studentCanRegisterToCourse(Student student, Course course) {
+        if (registrationRepository.existsByStudentAndCourse(student, course)) {
+            log.debug("Student with studentId {} is already registered to course with courseId: {}", student.getStudentId(), course.getCourseId());
+            return false;
+        }
+        if (registrationRepository.countByCourse(course) >= maximumNumberOfStudentsPerCourse) {
+            log.debug("Course with courseId {} cannot have more than {} registered student", course.getCourseId(), maximumNumberOfStudentsPerCourse);
+            return false;
+        }
+        if (registrationRepository.countByStudent(student) >= maximumNumberOfCoursesPerStudent) {
+            log.debug("Student with studentId {} cannot register to more than {} courses", student.getStudentId(), maximumNumberOfCoursesPerStudent);
+            return false;
+        }
+        return true;
+    }
 
     @PostConstruct
     public void postConstruct() {
