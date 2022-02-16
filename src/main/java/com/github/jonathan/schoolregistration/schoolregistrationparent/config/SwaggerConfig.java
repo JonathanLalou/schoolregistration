7288088535e87d982235aa121a2a54719a4fde2b
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2022-02-16 12:52:21 -0500

    added support for Swagger autogenerated doc.

---
+++ src/main/java/com/github/jonathan/schoolregistration/schoolregistrationparent/config/SwaggerConfig.java
@@ -0,0 +1,7 @@
+package com.github.jonathan.schoolregistration.schoolregistrationparent.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+}
